cbf483bd8cdd2cbc99359602a90b147c38c86a6e
Alex Burgel <aburgel@betterment.com>
2018-07-16 09:06:16 -0700

    Migrate some tests to jest

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,16 @@ module.exports = {
   },
   externals: {
     jquery: 'jQuery'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };

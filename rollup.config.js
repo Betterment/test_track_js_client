944f99b84a51911968ac6b47fbe6e660e53c6340
Nick Hehr <nicholas.hehr@betterment.com>
2020-07-24 18:09:06 -0400

    chore: use official typescript rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,48 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
+
+const customTSConfig = {
+  tsconfig: false,
+  include: ['src/*.ts', 'types'],
+  noEmit: true,
+  allowJs: true,
+  target: 'es5',
+  module: 'esnext',
+  lib: ['dom', 'esnext'],
+  importHelpers: true,
+  rootDir: 'src',
+  strict: true,
+  pretty: true,
+  noImplicitAny: true,
+  strictNullChecks: true,
+  strictFunctionTypes: true,
+  strictPropertyInitialization: true,
+  noImplicitThis: true,
+  alwaysStrict: true,
+  noUnusedLocals: true,
+  noUnusedParameters: true,
+  noImplicitReturns: true,
+  noFallthroughCasesInSwitch: true,
+  moduleResolution: 'node',
+  baseUrl: './',
+  paths: {
+    '*': ['src/*', 'node_modules/*']
+  },
+  esModuleInterop: true
+};
 
 export default [
   {
     input: 'src/testTrack.ts',
     external: ['js-cookie', 'uuid/v4', 'base-64', 'blueimp-md5', 'axios'],
     output: {
-      file: 'dist/testTrack.js',
+      dir: 'dist',
       format: 'esm'
     },
     plugins: [
-      typescript(),
+      typescript({ noEmitOnError: false }),
       commonjs(),
       babel({
         exclude: 'node_modules/**'
@@ -31,7 +61,7 @@ export default [
       resolve({
         browser: true
       }),
-      typescript(),
+      typescript(customTSConfig),
       commonjs(),
       terser(),
       babel({

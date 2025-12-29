b5ebc38d28bfe2d12fe4195f4e516e338ca89fb0
Ray Zane <raymondzane@gmail.com>
2025-12-29 14:45:07 -0500

    Fix eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,10 @@
+import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
-import js from '@eslint/js';
-import jest from 'eslint-plugin-jest';
-import tsParser from '@typescript-eslint/parser';
-import tseslint from '@typescript-eslint/eslint-plugin'
+import tseslint from 'typescript-eslint';
 
-export default defineConfig([
-	{
-		files: ['src/*.{js,ts}'],
-		plugins: {
-			js,
-			jest,
-			tseslint,
-		},
-		extends: [
-			'js/recommended',
-			'jest/recommended',
-			'tseslint/recommended',
-		],
-		rules: {
-			'no-prototype-builtins': 'off'
-		},
-		languageOptions: {
-			parser: tsParser,
-			parserOptions: {
-				ecmaVersion: 2020,
-				sourceType: 'module',
-			},
-			globals: {
-				window: 'writable',
-			}
-		},
-	}
-]);
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
+  files: ['src/**/*.ts'],
+  rules: {
+    'no-prototype-builtins': 'off'
+  }
+});

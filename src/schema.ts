5279f114559be79c42b7f981322161ec87b08274
Ray Zane <raymondzane@gmail.com>
2026-01-17 14:19:39 -0500

    Typecheck `splitName`

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,8 @@
 export type Schema = {
   serializer_version: 1;
   identifier_types: Array<{ name: string }>;
+  splits: Array<{ name: string; weights: Record<string, number> }>;
 };
 
 export type IdentifierType<S extends Schema> = S['identifier_types'][number]['name'];
+export type SplitName<S extends Schema> = S['splits'][number]['name'];

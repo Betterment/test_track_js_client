647d50bc03289106b7ccc0c425918588a316a814
Ray Zane <raymondzane@gmail.com>
2026-01-17 14:19:39 -0500

    Typecheck `splitName`

--- src/testTrack.test-d.ts
+++ src/testTrack.test-d.ts
@@ -15,6 +15,7 @@ describe('TestTrack', () => {
   test('ab', () => {
     expectTypeOf(testTrack.ab).parameter(0).toBeString();
     expectTypeOf(testTrack.ab).parameter(1).toEqualTypeOf<{ context: string; trueVariant?: string }>();
+    expectTypeOf(testTrack.ab).returns.toBeBoolean();
   });
 
   test('logIn', () => {
@@ -34,9 +35,25 @@ describe('TestTrack (typed)', () => {
   type ExampleSchema = {
     serializer_version: 1;
     identifier_types: [{ name: 'user_id' }, { name: 'agent_id' }];
+    splits: [
+      { name: 'foo_enabled'; weights: { true: 50; false: 50 } },
+      { name: 'color_experiment'; weights: { green: 50; blue: 50 } }
+    ];
   };
 
   const testTrack = {} as unknown as TestTrack<ExampleSchema>;
+
+  test('vary', () => {
+    expectTypeOf(testTrack.vary).parameter(0).toEqualTypeOf<'foo_enabled' | 'color_experiment'>();
+    expectTypeOf(testTrack.vary).parameter(1).toEqualTypeOf<{ context: string; defaultVariant: string | boolean }>();
+    expectTypeOf(testTrack.vary).returns.toBeString();
+  });
+
+  test('ab', () => {
+    expectTypeOf(testTrack.ab).parameter(0).toEqualTypeOf<'foo_enabled' | 'color_experiment'>();
+    expectTypeOf(testTrack.ab).parameter(1).toEqualTypeOf<{ context: string; trueVariant?: string }>();
+    expectTypeOf(testTrack.ab).returns.toBeBoolean();
+  });
 
   test('logIn', () => {
     expectTypeOf(testTrack.logIn).parameter(0).toEqualTypeOf<'user_id' | 'agent_id'>();

698ba89ddb65bebdc074b4e76ea24fab7b68dee4
Ray Zane <raymondzane@gmail.com>
2026-01-22 17:08:23 -0500

    chore: Export `stub` and `Splits` (#103)
    Forgot to export some things.
    
    /no-platform

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { initialize, load, create } from './api';
+export { initialize, load, create, stub } from './api';
 export { createCookieStorage } from './storageProvider';
 
 export type { TestTrack } from './testTrack';
 export type { AnalyticsProvider } from './analyticsProvider';
 export type { StorageProvider } from './storageProvider';
-export type { AnySchema, SplitName, VariantName, IdentifierType } from './schema';
+export type { AnySchema, Splits, SplitName, VariantName, IdentifierType } from './schema';

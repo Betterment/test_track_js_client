27788da12c3359289a616b4749d76ef3713d6ad5
Ray Zane <raymondzane@gmail.com>
2026-01-22 11:11:56 -0500

    Fix the base URL

--- src/client/index.ts
+++ src/client/index.ts
@@ -12,14 +12,20 @@ export * from './types';
 
 export type Client = ReturnType<typeof createClient>;
 
+export function urlFor(path: `/${string}`, base: URL): URL {
+  return new URL(base.pathname.replace(/\/$/, '') + path, base);
+}
+
 export function createClient(config: ClientConfig) {
-  const buildURL = `/api/v4/apps/${config.appName}/versions/${config.appVersion}/builds/${config.buildTimestamp}`;
+  const base = new URL(config.url);
+  const buildURL =
+    `/api/v4/apps/${config.appName}/versions/${config.appVersion}/builds/${config.buildTimestamp}` as const;
 
   return {
     async getVisitorConfig(visitorId: string): Promise<V4VisitorConfig> {
       const { data } = await request<V4VisitorConfig>({
         method: 'GET',
-        url: new URL(`${buildURL}/visitors/${visitorId}/config`, config.url),
+        url: urlFor(`${buildURL}/visitors/${visitorId}/config`, base),
         timeout: 5000
       });
 
@@ -29,7 +35,7 @@ export function createClient(config: ClientConfig) {
     async postIdentifier(params: V4IdentifierParams): Promise<V4VisitorConfig> {
       const { data } = await request<V4VisitorConfig>({
         method: 'POST',
-        url: new URL(`${buildURL}/identifier`, config.url),
+        url: urlFor(`${buildURL}/identifier`, base),
         body: JSON.stringify(params)
       });
 
@@ -39,7 +45,7 @@ export function createClient(config: ClientConfig) {
     async postAssignmentOverride({ auth, visitor_id, assignments }: V2AssignmentOverrideParams): Promise<void> {
       await request({
         method: 'POST',
-        url: new URL(`/api/v2/visitors/${visitor_id}/assignment_overrides`, config.url),
+        url: urlFor(`/api/v2/visitors/${visitor_id}/assignment_overrides`, base),
         body: JSON.stringify({ assignments }),
         auth
       });
@@ -48,7 +54,7 @@ export function createClient(config: ClientConfig) {
     async postAssignmentEvent(params: V1AssignmentEventParams): Promise<void> {
       await request({
         method: 'POST',
-        url: new URL('/api/v1/assignment_event', config.url),
+        url: urlFor('/api/v1/assignment_event', base),
         body: JSON.stringify(params)
       });
     }
